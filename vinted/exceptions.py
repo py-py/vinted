22360a213ed4aeb3ede32f3bcd8a7677861bd451
r.valeev <ruslan@dressx.com>
2026-04-01 01:49:28 +0200

    disable Markdown formatting

---
+++ vinted/exceptions.py
@@ -0,0 +1,8 @@
+class BadRequestHTTPException(Exception):
+    def __init__(self, status_code, message):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"Message: {self.message} [Code: {self.status_code}]"
--- vinted/telegram.py
+++ vinted/telegram.py
@@ -5,6 +5,7 @@ import os
 import httpx
 from dotenv import load_dotenv
 
+from .exceptions import BadRequestHTTPException
 from .models import VintedProduct
 
 load_dotenv()
@@ -19,15 +20,23 @@ async def send_message(
     analysis: str,
     parse_mode: str = "Markdown",
 ) -> None:
-    message = f"*{product.title}* [OPEN]({product.url})\n\n{analysis}"
+    message = f"▶ {product.title} ◀\n{product.url}\n{analysis}"
 
     async with httpx.AsyncClient() as client:
-        await client.post(
-            f"{API_URL}/sendPhoto",
-            json={
-                "chat_id": CHAT_ID,
-                "caption": message,
-                "parse_mode": parse_mode,
-                "photo": product.main_image_url,
-            },
-        )
+        try:
+            reply = await client.post(
+                f"{API_URL}/sendPhoto",
+                json={
+                    "chat_id": CHAT_ID,
+                    "caption": message,
+                    "photo": product.main_image_url,
+                },
+            )
+            reply.raise_for_status()
+        except httpx.HTTPStatusError:
+            if reply.status_code == 400:
+                raise BadRequestHTTPException(
+                    message=reply.text,
+                    status_code=reply.status_code,
+                )
+            raise
